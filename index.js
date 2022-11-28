c347597376b12314c47e1689477a60ad70fed7fa
eliasndegwa <eliasndegwamwai@gmail.com>
2022-11-28 14:04:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName="bob"
+function upperCaseCustomerName(){
+    customerName = (customerName.toUpperCase())
+}
+var bestCustomer;
+function setBestCustomer(){
+  return (bestCustomer = 'not bob')
+}
+function overwriteBestCustomer(){
+    return (bestCustomer = 'maybe bob')
+}
+const leastFavoriteCustomer="John"
+function changeLeastFavoriteCustomer(){
+     leastFavoriteCustomer = 'not John'
+}
